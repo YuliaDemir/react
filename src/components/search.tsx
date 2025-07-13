f214afaff9d39c0c8f1d7d0defaa3f9b66d205fb
Yulia  Demir <Nightelfin91@gmail.com>
2025-07-13 13:18:08 +0300

    feat: add search component

---
+++ src/components/search.tsx
@@ -0,0 +1,29 @@
+import { Component } from "react";
+
+interface SearchState {
+    value: string;
+}
+
+interface SearchProps {
+    value: string;
+    onSearch (query: string) :void;
+}
+
+
+class Search extends Component<SearchProps, SearchState>{
+
+    state = {
+        value: this.props.value || "",
+    }
+
+    render() {
+        return(
+            <div>
+                <input type="text" value={this.state.value} onChange={(e) => this.setState({value: e.target.value.trim()})}></input>
+                <button onClick={() => this.props.onSearch(this.state.value)}>Search</button>
+            </div>
+        );
+    }
+}
+
+export default Search;
