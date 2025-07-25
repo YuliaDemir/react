01eaf22bb9a3d350344deeb6dc7bc2c173712fdd
Yulia  Demir <Nightelfin91@gmail.com>
2025-07-25 09:40:11 +0300

    feat: change url to img for pokemons card

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,13 +1,30 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router';
 
-export const Card = (props: { name: string; description: string }) => {
-  const { name, description } = props;
+export const Card = ({
+  name,
+  description,
+}: {
+  name: string;
+  description: string;
+}) => {
+  const [imgUrl, setImgUrl] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    (async () => {
+      const pokData = await fetch(description).then((res) => res.json());
+      const formUrl = pokData.forms[0].url;
+      const formData = await fetch(formUrl).then((res) => res.json());
+      setImgUrl(formData.sprites.front_default);
+    })();
+  }, [description]);
+
   return (
     <div className="card-row">
       <Link to={`/${name}`} className="card-name">
         {name}
       </Link>
-      <div className="card-description">{description}</div>
+      <img src={imgUrl} />
     </div>
   );
 };
