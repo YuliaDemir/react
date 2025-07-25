3f12328d5aad12a5f53fe774359614a87c10a533
Yulia  Demir <Nightelfin91@gmail.com>
2025-07-25 19:26:56 +0300

    feat: apdate about page

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -2,9 +2,29 @@ import { Link } from 'react-router';
 
 export const About = () => {
   return (
-    <>
-      <Link to="/">Home</Link>
-      ABOUT
-    </>
+    <div className="max-w-2xl mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-4">About</h1>
+      <p className="=mb-2 text-lg">
+        Created by <span className="font-semibold">Yulia Demir</span>.
+      </p>
+      <p className="mb-4 text-lg">
+        This app was developed as part of the{' '}
+        <Link
+          to="https://rs.school/courses/reactjs"
+          className="text-blue-600 underline hover:text-blue-800"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          RS School React course
+        </Link>
+        .
+      </p>
+      <Link
+        to="/"
+        className="inline-block mt-4 px-4 py-2 bg-blue-500 text-white rounded-2xl hover:bg-blue-600"
+      >
+        Back to Home
+      </Link>
+    </div>
   );
 };
