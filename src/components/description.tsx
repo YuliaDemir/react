38a28dbdcb1926ea60ac9826a81fd01b3a77713d
Yulia  Demir <Nightelfin91@gmail.com>
2025-07-25 21:28:12 +0300

    fix: add outside-click close posibility

--- src/components/description.tsx
+++ src/components/description.tsx
@@ -1,6 +1,6 @@
-import { Link, useParams } from 'react-router';
+import { Link, useNavigate, useParams } from 'react-router';
 import { LINK } from './helpers/consts';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Loader } from './loader';
 
 interface AbilityItem {
@@ -20,6 +20,22 @@ function findAbilityDescriptionEn(
 }
 
 export const Description = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        navigate('/');
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [navigate]);
+
   const { index } = useParams();
   const [abilities, setAbilities] = useState<
     { name: string; description: string }[] | null
@@ -52,7 +68,10 @@ export const Description = () => {
     return <Loader />;
   }
   return (
-    <div className="relative bg-white shadow-lg rounded-2xl p-6 w-full max-w-md mx-auto mt-4">
+    <div
+      ref={ref}
+      className="relative bg-white shadow-lg rounded-2xl p-6 w-full max-w-md mx-auto mt-4"
+    >
       <Link
         to="/"
         className="absolute top-2 right-2 text-gray-700 hover:text-red-600 transition"
@@ -61,13 +80,13 @@ export const Description = () => {
         ✖
       </Link>
 
-      <h2 className="text--xl font-bold text-gray-800 mb-4">
+      <h2 className="text-xl font-bold text-gray-800 mb-4">
         Abilities of {index?.toUpperCase()}
       </h2>
       <ul className="space-y-2">
         {abilities.map((a) => (
-          <li key={a.name} className="test-sm text-gray-700">
-            <strong className="test-sm text-gray-900">{a.name}</strong>:{' '}
+          <li key={a.name} className="text-sm text-gray-700">
+            <strong className="text-sm text-gray-900">{a.name}</strong>:{' '}
             {a.description}
           </li>
         ))}
