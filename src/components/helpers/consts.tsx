b5edba89e05e6c8c9f051377774e8100832a2eea
Yulia  Demir <Nightelfin91@gmail.com>
2025-08-03 17:13:26 +0300

    style: add fix height for card list

--- src/components/helpers/consts.tsx
+++ src/components/helpers/consts.tsx
@@ -1,5 +1,5 @@
 export const OFFSET_KEY = 'offset';
 export const LIMIT_KEY = 'limit';
-export const LIMIT_NUMBER = 5;
+export const LIMIT_NUMBER = 10;
 export const LINK = 'https://pokeapi.co/api/v2/pokemon/';
 export const MAX = 1302;
