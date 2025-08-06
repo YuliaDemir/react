8b81ba0e4a51be28620f45a69f4eb5f1889d9375
Yulia  Demir <Nightelfin91@gmail.com>
2025-08-06 16:25:55 +0300

    feat: improve loader

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -1,7 +1,10 @@
-export const Loader = () => {
+export const Loader = ({ size = 'w-10 h-10', color = 'border-blue-500' }) => {
   return (
-    <div>
-      <span>Loading...</span>
+    <div className="flex items-center justify-center">
+      <div
+        className={`animate-spin inline-block ${size} border-4 border-solid rounded-full border-t-transparent ${color}`}
+        role="status"
+      />
     </div>
   );
 };
