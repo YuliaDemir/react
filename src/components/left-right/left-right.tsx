107572262f4c98bc540bbcd26165ad5a1899c01a
Yulia  Demir <Nightelfin91@gmail.com>
2025-08-10 17:46:49 +0300

    feat: refetch button

--- src/components/left-right/left-right.tsx
+++ src/components/left-right/left-right.tsx
@@ -1,6 +1,8 @@
+import { useDispatch } from 'react-redux';
 import { twMerge } from 'tailwind-merge';
 
 import { LIMIT_NUMBER, MAX } from '@/constants';
+import { api } from '@/features/slices/api-slice';
 export const LeftRight = ({
   curPage,
   setPage,
@@ -8,6 +10,8 @@ export const LeftRight = ({
   curPage: string;
   setPage: (newPage: number) => void;
 }) => {
+  const dispatch = useDispatch();
+
   function navigate(curPage: number, left: boolean) {
     const newPage = curPage - (left ? LIMIT_NUMBER : -LIMIT_NUMBER);
     setPage(newPage);
@@ -24,6 +28,17 @@ export const LeftRight = ({
         disabled={Number(curPage) <= 0}
       >
         Left
+      </button>
+      <button
+        className={twMerge(
+          'bg-gray-300  dark:bg-blue-500 hover:bg-gray-400',
+          'px-4 py-2 rounded-2xl disabled:opacity-50'
+        )}
+        onClick={() =>
+          dispatch(api.util.invalidateTags(['Pokemon', 'Ability']))
+        }
+      >
+        Refetch
       </button>
       <button
         className={twMerge(
