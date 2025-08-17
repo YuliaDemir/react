9377d96fdf3e0cb70e9e61c4cb2055d290c789ff
Yulia  Demir <Nightelfin91@gmail.com>
2025-08-17 15:09:15 +0300

    feat:msmth

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,17 +1,7 @@
-import { locales, type Locale } from '@/constants';
+import type { Locale } from '@/constants';
 import { Providers } from '@/features/providers';
 
-import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
-
-export const metadata: Metadata = {
-  title: 'Pokemons',
-  description: 'Description of pokemons',
-};
-
-export function generateStaticParams() {
-  return locales.map((locale) => ({ locale }));
-}
 
 export default async function RootLayout({
   children,
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,17 @@
-import '@/globals.css';
+import './index.css'
 
+import { locales } from '@/constants';
+
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Pokemons',
+  description: 'Description of pokemons',
+};
+
+export function generateStaticParams() {
+  return locales.map((locale) => ({ locale }));
+}
 export default function StylesLayout({
   children,
 }: {
